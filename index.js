c32fe684251f0f21a77ce1440279d7d3fa68bb4d
alextaylor <alextaylor515@gmail.com>
2022-06-14 13:33:00 -0400

    commit tree 25219d8019fddf80df74dd52a94abe67400f8091

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 /* Your Code Here */
+// Your code here
+
+function createEmployeeRecord(arr){
+    return {firstName: arr[0],familyName: arr[1], title: arr[2], payPerHour: arr[3],timeInEvents:[],timeOutEvents:[]}
+}
+function createEmployeeRecords(arr){
+    const nArr = []
+    arr.forEach(item => {
+        nArr.push(createEmployeeRecord(item))
+    });
+    return nArr
+}
+function createTimeInEvent(date){
+    console.log(date)
+    const dates = date.split(" ")
+    this.timeInEvents.push
+        ({
+        type : "TimeIn",
+        hour: parseInt(dates[1]),
+        date: dates[0]
+        })
+
+    return this
+
+}
+function createTimeOutEvent(date){
+    const dates = date.split(" ")
+    this.timeOutEvents.push 
+        ({
+        type : "TimeOut",
+        hour: parseInt(dates[1]),
+        date: dates[0]
+        })
+    return this
+}
+function hoursWorkedOnDate(date){
+    let timeIn = this.timeInEvents.find((log)=>log.date === date)
+    let timeOut = this.timeOutEvents.find((log)=>log.date === date)
+
+    return (timeOut.hour - timeIn.hour) / 100
+}
+function wagesEarnedOnDate(date){
+    //Add context to hoursworkedondate, so wagesearnedondate and hoursworkedondate refer to same object
+    return (hoursWorkedOnDate.call(this, date) * this.payPerHour)
+}
+
+function calculatePayroll(obj){
+    let count = 0
+    obj.forEach(employee=>{
+        count += allWagesFor(employee)    
+    })
+    return count
+}
+function findEmployeeByFirstName(srcArray,firstName) {
+    return srcArray.find(item=> item.firstName ===firstName)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +77,7 @@ const allWagesFor = function () {
 
     return payable
 }
-
+function calculatePayroll(records){
+    //console.log(records.reduce((prevRecord, record) => prevRecord + allWagesFor.call(record)))
+    return (records.reduce((prevRecord, record) => prevRecord +  allWagesFor.call(record),0))
+}
